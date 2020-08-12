0518e4cdbfee103aa57c9377ac6e67edef272f40
Vaibhav Satam <vaibhav.satam29991@gmail.com>
2020-08-13 00:43:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 )
 
 func main() {
-	// numbers := []int{1, 2, 3, 4, 7}
-	// fmt.Println(leetcode.HasDuplicates(numbers))
 	str := "golang dev"
 	fmt.Println(leetcode.ReverseString(str))
 }
